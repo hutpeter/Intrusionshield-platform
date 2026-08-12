b899e66511ce19dc91af5b9adb21c2d9acf832e8
hutpeter <peter@hut-6.com>
2026-08-12 17:27:44 -0400

    test(config): add Jest ESM TypeScript configuration

---
+++ packages/config/jest.config.mjs
@@ -0,0 +1,54 @@
+export default {
+    rootDir: ".",
+
+    testEnvironment: "node",
+
+    extensionsToTreatAsEsm: [".ts"],
+
+    transform: {
+        "^.+\\.tsx?$": [
+            "ts-jest",
+            {
+                useESM: true,
+                tsconfig: "<rootDir>/tsconfig.json"
+            }
+        ]
+    },
+
+    testMatch: [
+        "<rootDir>/src/**/*.test.ts"
+    ],
+
+    testPathIgnorePatterns: [
+        "/node_modules/",
+        "/dist/"
+    ],
+
+    collectCoverageFrom: [
+        "src/**/*.ts",
+        "!src/**/*.test.ts",
+        "!src/types/**",
+        "!src/index.ts"
+    ],
+
+    coverageDirectory: "coverage",
+
+    coverageReporters: [
+        "text",
+        "text-summary",
+        "lcov"
+    ],
+
+    moduleFileExtensions: [
+        "ts",
+        "tsx",
+        "js",
+        "mjs"
+    ],
+
+    clearMocks: true,
+
+    restoreMocks: true,
+
+    verbose: true
+};
