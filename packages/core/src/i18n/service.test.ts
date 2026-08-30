a086ff1c9e580694176e0690e3a613db0ed0f236
hutpeter <peter@hut-6.com>
2026-08-30 08:43:36 -0400

    test(core): use Jest globals for i18n tests

--- packages/core/src/i18n/service.test.ts
+++ packages/core/src/i18n/service.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it } from "@jest/globals";
 import { DefaultI18nService } from "./service.js";
 
 describe("DefaultI18nService", () => {
