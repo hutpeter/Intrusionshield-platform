03e96e8906d0f2ba0c0c664b382eb0f7d2792323
hutpeter <peter@hut-6.com>
2026-08-30 09:29:57 -0400

    fix(common): implement phone number value object

--- packages/common/src/value-objects/PhoneNumber.ts
+++ packages/common/src/value-objects/PhoneNumber.ts
@@ -1,5 +1,19 @@
-PhoneNumber.create(
+import { ValueObject } from "./ValueObject.js";
 
-"+1 613-555-1212"
+export class PhoneNumber extends ValueObject<string> {
+    private constructor(value: string) {
+        super(value);
+    }
 
-);
+    public static create(value: string): PhoneNumber {
+        const phone = value?.trim();
+        if (!phone || !/^\+?[1-9]\d{7,14}$/.test(phone.replace(/[\s().-]/g, ""))) {
+            throw new Error("Invalid phone number.");
+        }
+        return new PhoneNumber(phone);
+    }
+
+    public toString(): string {
+        return this.value;
+    }
+}
