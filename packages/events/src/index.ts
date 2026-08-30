e16735a1fae990804de8a1404df881e94b44960c
hutpeter <peter@hut-6.com>
2026-08-30 09:14:27 -0400

    feat(events): expose public event package API

---
+++ packages/events/src/index.ts
@@ -0,0 +1,10 @@
+export * from "./EventBus.js";
+export * from "./EventTypes.js";
+
+export * from "./abstractions/IEvent.js";
+export * from "./abstractions/IEventBus.js";
+export * from "./abstractions/IEventHandler.js";
+export * from "./abstractions/IEventPublisher.js";
+
+export * from "./models/Event.js";
+export * from "./models/EventMetadata.js";
