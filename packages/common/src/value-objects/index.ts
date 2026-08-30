a55393a27c163061ef386515248e5e868b06cdd6
hutpeter <peter@hut-6.com>
2026-08-30 09:29:49 -0400

    fix(common): add value object barrel exports

---
+++ packages/common/src/value-objects/index.ts
@@ -0,0 +1,7 @@
+export * from "./ValueObject.js";
+export * from "./Identifier.js";
+export * from "./CorrelationId.js";
+export * from "./EmailAddress.js";
+export * from "./PhoneNumber.js";
+export * from "./money.js";
+export * from "./percentage.js";
