344135d50b0ab72d4c7f7febd4bd3e29e6389f7c
hutpeter <peter@hut-6.com>
2026-09-08 09:16:41 -0400

    feat(his): expose HIS domain foundation

---
+++ packages/his/src/index.ts
@@ -0,0 +1,5 @@
+export * from "./shared.js";
+export * from "./persons/index.js";
+export * from "./patients/index.js";
+export * from "./practitioners/index.js";
+export * from "./facilities/index.js";
